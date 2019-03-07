f1fa3327130dc137f00fe451db80fb7631eec347
Marek <marekficht@gmail.com>
2019-03-07 18:51:17 +0100

    First layout with RWD.

---
+++ src/containers/App/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <h1>Witam</h1>
+
+        <div className="container">
+          <div className="grid">
+            <div className="grid-item grid-item-1">O MNIE</div>
+            <div className="grid-item grid-item-2">UMIEJĘTNOŚCI</div>
+            <div className="grid-item grid-item-3">PROJEKTY</div>
+            <div className="grid-item grid-item-4">CV</div>
+            <div className="grid-item grid-item-5">KONTAKT</div>
+            <div className="grid-item grid-item-6">P1</div>
+            <div className="grid-item grid-item-7">P2</div>
+            <div className="grid-item grid-item-8">P3</div>
+            <div className="grid-item grid-item-9">P4</div>
+            <div className="grid-item grid-item-10">P5</div>
+            <div className="grid-item grid-item-11">P6</div>
+          </div>
+        </div>
+
+        <footer> Marek Ficht &copy; ALL RIGHTS RESERVED </footer>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App/App';
+import './index.css';
+
+// import { Route, HashRouter, Switch, Link } from 'react-router-dom';
+
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCheckCircle, faTimesCircle, faVolumeUp, faVolumeOff } from '@fortawesome/free-solid-svg-icons'
+// library.add(faCheckCircle, faTimesCircle, faVolumeUp, faVolumeOff);
+
+ReactDOM.render(<App />, document.getElementById('root'));
