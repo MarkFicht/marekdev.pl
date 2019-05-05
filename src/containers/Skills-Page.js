d6f87ff85a283ffc358e138dbba8fc6110600ba6
Marek <marekficht@gmail.com>
2019-05-05 16:43:34 +0200

    First layout skills page

--- src/containers/Skills-Page.js
+++ src/containers/Skills-Page.js
@@ -5,7 +5,18 @@ import jsImg from '../img/js-logo.png'
 import htmlImg from '../img/html-logo.png'
 import cssImg from '../img/css-logo.png'
 import otherImg from '../img/other-logo (3).png'
+import starG from '../img/star-g.png'
+import starS from '../img/star-s.png'
+import starB from '../img/star-b.png'
 
+/**
+ * Images from -> https://icons8.com
+ */
+
+const jsTechList = ['NPM', 'React, jQuery', 'ES6, Babel, Bundlers', 'Design patterns']
+ const htmlTechList = ['HTML5', 'Mobile first', 'Semantic', 'SEO']
+ const cssTechList = ['Sass', 'Grid & Flex', 'RWD', 'Bootstrap']
+ const otherTechList = ['FireBase', 'REST API', 'JSON & Json-server', 'ExpressJS']
 
 class Skills extends Component {
 
@@ -14,14 +25,25 @@ class Skills extends Component {
     }
 
     componentDidMount() {
-        window.scroll(0, 0);
+        window.scroll(0, 0)
 
         const loadingTime = setTimeout( () => {
 
             this.setState({ loading: false })
-            clearTimeout(loadingTime);
-        }, 500 );
+            clearTimeout(loadingTime)
+        }, 500 )
     }
+
+    divLogoTech = logo => <div className='svg-technology' style={{ backgroundImage: `url(${logo ? logo : jsImg})` }}></div>
+    divStar = star => <div className='star-technology' style={{ backgroundImage: `url(${star ? star : starG})` }}></div>
+    divListTech = techList => (
+        <div className='description-technology'>
+            { techList 
+                ? techList.map( tech => <p>{tech}</p> )
+                : null 
+            }
+        </div>
+    )
 
     render() {
 
@@ -47,14 +69,9 @@ class Skills extends Component {
                                 <div className='bg-technology'>
                                     <div className='bg-technology__text'>JS</div>
                                 </div>
-                                <div className='svg-technology' style={{ backgroundImage: `url(${jsImg})` }}></div>
-                                <div className='star-technology'>gold</div>
-                                <div className='description-technology'>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                </div>
+                                { this.divLogoTech(jsImg) }
+                                { this.divStar(starS) }
+                                { this.divListTech(jsTechList) }
                             </div>
                         </div>
 
@@ -63,14 +80,9 @@ class Skills extends Component {
                                 <div className='bg-technology'>
                                     <div className='bg-technology__text'>HTML</div>
                                 </div>
-                                <div className='svg-technology' style={{ backgroundImage: `url(${htmlImg})` }}></div>
-                                <div className='star-technology'>gold</div>
-                                <div className='description-technology'>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                </div>
+                                { this.divLogoTech(htmlImg) }
+                                { this.divStar(starG) }
+                                { this.divListTech(htmlTechList) }
                             </div>
                         </div>
 
@@ -79,14 +91,9 @@ class Skills extends Component {
                                 <div className='bg-technology'>
                                     <div className='bg-technology__text'>CSS</div>
                                 </div>
-                                <div className='svg-technology' style={{ backgroundImage: `url(${cssImg})` }}></div>
-                                <div className='star-technology'>gold</div>
-                                <div className='description-technology'>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                </div>
+                                { this.divLogoTech(cssImg) }
+                                { this.divStar(starG) }
+                                { this.divListTech(cssTechList) }
                             </div>
                         </div>
 
@@ -95,14 +102,9 @@ class Skills extends Component {
                                 <div className='bg-technology'>
                                     <div className='bg-technology__text'>Inne</div>
                                 </div>
-                                <div className='svg-technology' style={{ backgroundImage: `url(${otherImg})` }}></div>
-                                <div className='star-technology'>gold</div>
-                                <div className='description-technology'>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                    <p>React</p>
-                                </div>
+                                { this.divLogoTech(otherImg) }
+                                { this.divStar(starB) }
+                                { this.divListTech(otherTechList) }
                             </div>
                         </div>
 
@@ -113,4 +115,4 @@ class Skills extends Component {
     }
 }
 
-export default Skills;
+export default Skills
